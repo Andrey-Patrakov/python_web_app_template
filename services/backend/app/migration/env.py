2a44ec24dc2a32df46165cb4151020e80250b199
Patrakov Andrey Vladimirovich <a.v.patrakov95@gmail.com>
2025-04-05 19:23:38 +0300

    prevent "alembic revision --autogenerate" from making empty revision file

--- services/backend/app/migration/env.py
+++ services/backend/app/migration/env.py
@@ -66,7 +66,17 @@ def run_migrations_offline() -> None:
 
 
 def do_run_migrations(connection: Connection) -> None:
-    context.configure(connection=connection, target_metadata=target_metadata)
+    def process_revision_directives(context, revision, directives):
+        if config.cmd_opts.autogenerate:
+            script = directives[0]
+            if script.upgrade_ops.is_empty():
+                directives[:] = []
+                print('No changes in schema detected.')
+
+    context.configure(
+        connection=connection,
+        target_metadata=target_metadata,
+        process_revision_directives=process_revision_directives)
 
     with context.begin_transaction():
         context.run_migrations()
