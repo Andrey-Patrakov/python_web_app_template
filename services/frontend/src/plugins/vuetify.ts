f05548aa6c69858743ca25b93f7e0f73979c5280
Patrakov Andrey Vladimirovich <a.v.patrakov95@gmail.com>
2025-05-09 19:14:20 +0300

    added localization

--- services/frontend/src/plugins/vuetify.ts
+++ services/frontend/src/plugins/vuetify.ts
@@ -10,9 +10,22 @@ import 'vuetify/styles'
 
 // Composables
 import { createVuetify } from 'vuetify'
+import DateFnsAdapter from '@date-io/date-fns'
+import { ru } from 'date-fns/locale'
+import { ru as messages_ru, en } from 'vuetify/locale'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
+  locale: {
+    locale: 'ru',
+    messages: {ru: messages_ru, en},
+  },
+  date: {
+    adapter: DateFnsAdapter,
+    locale: {
+      ru: ru
+    }
+  },
   theme: {
     defaultTheme: 'light',
     themes: {
